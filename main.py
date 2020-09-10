b3c70e477aa606dd0cf239bc2f0b54859f03fd5a
void-memories <kiethberry@gmail.com>
2020-09-10 16:43:17 +0530

    init

---
+++ main.py
@@ -0,0 +1,22 @@
+import time
+from playsound import playsound
+from threading import Thread
+
+
+def metronome():
+    while 1:
+        playsound('chordWavs/stick.wav',False)
+        time.sleep(2.5/4)
+
+def chords():
+    queue=['F','Am','C','G']
+    while 1:
+        for i in queue:
+            playsound(f'chordWavs/90BPM/{i}.wav',False)
+            time.sleep(2.5)
+            #2==120BPM
+            #2.5==90BPM
+
+if __name__ == '__main__':
+    # Thread(target = metronome).start()
+    Thread(target = chords).start()
